929b1ad3c46a7a064997a6cdd30129b5016ad08b
MeKHell <MeKHell@MeKHell-debian.home>
2025-04-20 19:04:54 +0200

    Add launch script

---
+++ oss.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+yes | python3 infra/helper.py build_image libpng
+yes | python3 infra/helper.py build_fuzzers libpng
+mkdir -p build/out/corpus{1..10}
+for i in {1..10};
+do
+        python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus$i > /dev/null 2>&1 &
+done;
+for i in {1..24};
+do
+        sleep 1s
+        echo "`date --rfc-3339=seconds` - has been running for $i hours with `docker ps -q | wc -l` runners"
+done
+docker stop `docker ps -q`
+
+echo "Program completed. `docker ps -q | wc -l` containers are still running"
