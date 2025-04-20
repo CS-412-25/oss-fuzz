8e28476a07f1517522195a4857a1881a18388868
MeKHell <MeKHell@MeKHell-debian.home>
2025-04-20 19:09:39 +0200

    more verbose script

--- oss.sh
+++ oss.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 yes | python3 infra/helper.py build_image libpng
 yes | python3 infra/helper.py build_fuzzers libpng
+echo "Creating the output directories"
 mkdir -p build/out/corpus{1..10}
+echo "Launching the fuzzers"
 for i in {1..10};
 do
         python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus$i > /dev/null 2>&1 &
 done;
+echo "Fuzzers are running... Let's wait 24 hours now."
 for i in {1..24};
 do
         sleep 1s
