ad1e9be611edc2727737e5ac2a28157934e7e049
MeKHell <MeKHell@MeKHell-debian.home>
2025-05-01 16:29:15 +0200

    Save logs

--- oss.sh
+++ oss.sh
@@ -6,7 +6,7 @@ mkdir -p build/out/corpus{1..10}
 echo "Launching the fuzzers"
 for i in {1..10};
 do
-        python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus$i > /dev/null 2>&1 &
+        python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus$i > build/out/log$1 2>&1 &
 done;
 echo "Fuzzers are running... Let's wait 24 hours now."
 for i in {1..24};
