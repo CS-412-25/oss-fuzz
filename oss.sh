178c69e0ea857a3363394da63ecbfff63bf8e7c1
MeKHell <MeKHell@MeKHell-debian.home>
2025-05-14 07:43:38 +0200

    Fix oss-fuzz using the right fuzzer

--- oss.sh
+++ oss.sh
@@ -7,19 +7,19 @@ echo "Launching the fuzzers"
 pids=()
 for i in {0..9};
 do
-        out=$(python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus$i  &> build/out/log$i & echo $!)
+        out=$(python3 infra/helper.py run_fuzzer libpng libpng_transformation_fuzzer --corpus-dir build/out/corpus$i  &> build/out/log$i & echo $!)
         pids+=($out)
 
 done;
 echo ${pids[@]}
-echo "Fuzzers are running... Let's wait 24 hours now."
-for i in {1..1440};
+echo "Fuzzers are running... Let's wait 4 hours now."
+for i in {1..240};
 do
         sleep 1m
         echo "`date --rfc-3339=seconds` - has been running for $i minutes with `docker ps -q | wc -l` runners"
         for pidi in ${!pids[@]};do
                 if  ! [ -d "/proc/${pids[$pidi]}" ]; then
-                        out=$(python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus${pidi}  &>> build/out/log${pidi} & echo $!)
+                        out=$(python3 infra/helper.py run_fuzzer libpng libpng_transformation_fuzzer --corpus-dir build/out/corpus${pidi}  &>> build/out/log${pidi} & echo $!)
                         pids[${pidi}]=$out
                 fi
         done
